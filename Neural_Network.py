740c8bd24843cb06b6f0903355d14a20b10a4173
ckelly8 <70236589+ckelly8@users.noreply.github.com>
2023-06-14 23:33:11 -0400

    Add files via upload

--- Neural_Network.py
+++ Neural_Network.py
@@ -4,10 +4,11 @@ class NeuralNetwork(tf.keras.Model):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
         #input layer defines NN points 'vision'
-        self.input_layer = tf.keras.layers.Dense(8, activation='relu')
+        self.input_layer = tf.keras.layers.Dense(25, activation='relu',input_shape=(5,))
         #simple 2 layer wide neural network
-        self.hidden_layer1 = tf.keras.layers.Dense(18,activation='relu')
-        self.hidden_layer2 = tf.keras.layers.Dense(8,activation='relu')
+        self.hidden_layer1 = tf.keras.layers.Dense(50,activation='relu')
+        self.hidden_layer2 = tf.keras.layers.Dense(25,activation='relu')
+        self.hidden_layer3 = tf.keras.layers.Dense(5,activation='relu')
         #output is change in velocity for x and y dimensions
         self.output_layer = tf.keras.layers.Dense(2, activation='tanh')
     
@@ -15,5 +16,6 @@ class NeuralNetwork(tf.keras.Model):
         x = self.input_layer(x)
         x = self.hidden_layer1(x)
         x = self.hidden_layer2(x)
+        x = self.hidden_layer3(x)
         x = self.output_layer(x)
         return x
--- Particle_Dodge.py
+++ Particle_Dodge.py
@@ -5,25 +5,29 @@ import tensorflow as tf
 import Neural_Network
 import os
 
+#debugging
+import time
+
 # dimensions of the window
 WIDTH, HEIGHT = 600, 600
 # number of blue points
-N_POINTS = 10
+N_POINTS = 16
 # radius of points
-RADIUS = 10
+RADIUS = 5
 # color of network controlled point
 NN_POINT_COLOR = (255,0,0)
 # color of elastic collision / random point
 COLLISION_POINT_COLOR = (0,0,255)
 # max speed of points in simulation
-MAX_SPEED = 10
+MAX_SPEED = 5
 # number of neural networks in a population
-POPULATION_SIZE = 3
+POPULATION_SIZE = 5
 
 # path to current best network weights
 BEST_NETWORK = 'model_weights.h5'
-# sample neural network input. Will change later.
-NN_INPUT = np.random.rand(1,8)
+# How many points the NN_Point can 'see' at a time
+# based on how close those points are
+VISUAL_FIELD = 4
 
 # Network_Pool Class
 # This object will initialize a random population, track the best performers, and spawn
@@ -32,8 +36,22 @@ class Network_Pool:
     def __init__(self):
         self.population = []
 
-        for i in range(POPULATION_SIZE):
+        for _ in range(POPULATION_SIZE):
             self.population.append(NN_Point())
+
+    # fix for resetting population as bug occurs when carrying initial population 
+    # of NN_Point objects over to next population cycle
+    def reset_population(self):
+        BrainBasket = []
+        for i in range(POPULATION_SIZE):
+            BrainBasket.append(self.population[i].brain)
+        
+        self.population = []
+        for i in range(POPULATION_SIZE):
+            nn_point = NN_Point()
+            nn_point.brain = BrainBasket[i]
+            self.population.append(nn_point)
+
 
     def save_best(self):
         self.population[0].brain.save_weights('model_weights.h5')
@@ -48,7 +66,7 @@ class Network_Pool:
     def crossover(self, network1, network2):
         # Create a new instance of the network
         child_network = Neural_Network.NeuralNetwork()
-        child_network.build((None,8))
+        child_network.build((5,5))
 
         # Get the weights of the parent networks
         parent1_weights = network1.get_weights()
@@ -75,7 +93,7 @@ class Network_Pool:
         for _ in range(num_offspring):
             # Create a new instance of the network
             offspring = Neural_Network.NeuralNetwork()
-            offspring.build((None,8))
+            offspring.build((5,5))
             offspring.set_weights(parent_network.get_weights())
 
             # Perform mutation on the offspring network's weights
@@ -93,16 +111,33 @@ class Network_Pool:
     def mutate_population(self, mutated_brains):
         for i in range(len(mutated_brains)):
             self.population[i].brain = mutated_brains[i]
-
-    def reset_population_attributes(self):
-        for i in range(len(self.population)):
-            self.population[i].reset_position()
     
 # This object spawns mostly elastic particles with slight random behavior.
 class Collision_Point:
     def __init__(self):
-        self.x = random.uniform(RADIUS, WIDTH-RADIUS)
-        self.y = random.uniform(RADIUS, HEIGHT-RADIUS)
+        initialize_start = random.uniform(0,1)
+        print(initialize_start)
+
+        #top right corner
+        if initialize_start <= 0.25:
+            self.x = random.uniform(WIDTH-2*RADIUS, WIDTH-RADIUS)
+            self.y = random.uniform(RADIUS, 2*RADIUS)
+
+        #bottom right corner
+        if initialize_start > 0.25 and initialize_start <= 0.5:
+            self.x = random.uniform(WIDTH-2*RADIUS, WIDTH-RADIUS)
+            self.y = random.uniform(HEIGHT-2*RADIUS, HEIGHT-RADIUS)
+
+        #bottom left corner
+        if initialize_start > 0.5 and initialize_start <= 0.75:
+            self.x = random.uniform(RADIUS, 2*RADIUS)
+            self.y = random.uniform(HEIGHT-2*RADIUS, HEIGHT-RADIUS)
+        
+        #top left corner
+        if initialize_start > 0.75 and initialize_start <= 1:
+            self.x = random.uniform(RADIUS, 2*RADIUS)
+            self.y = random.uniform(RADIUS, 2*RADIUS)
+
         self.vx = random.uniform(-MAX_SPEED, MAX_SPEED)
         self.vy = random.uniform(-MAX_SPEED, MAX_SPEED)
         self.color = COLLISION_POINT_COLOR
@@ -118,25 +153,21 @@ class Collision_Point:
             self.vx = -self.vx # + random.uniform(0,1)
             #if self.vx >= 5:
             #    self.vx = random.uniform(0,MAX_SPEED)
-            print('boundary')
 
         if self.x + RADIUS > WIDTH:
             self.vx = -self.vx # - random.uniform(0,2)
             #if self.vx <= -5:
             #    self.vx = random.uniform(-MAX_SPEED,0)
-            print('boundary')
 
         if self.y - RADIUS < 0:
             self.vy = -self.vy # + random.uniform(0,2)
             #if self.vy >= 5:
             #    self.vy = random.uniform(0,MAX_SPEED)
-            print('boundary')
 
         if self.y + RADIUS > HEIGHT:
             self.vy = -self.vy # - random.uniform(0,2)
             #if self.vy <= -5:
             #    self.vy = random.uniform(-MAX_SPEED,0)
-            print('boundary')
 
     def draw(self, screen):
         pygame.draw.circle(screen, self.color, (int(self.x), int(self.y)), RADIUS)
@@ -146,44 +177,52 @@ class NN_Point:
     def __init__(self):
         self.x = WIDTH/2 #random.uniform(RADIUS, WIDTH-RADIUS)
         self.y = HEIGHT/2 #random.uniform(RADIUS, HEIGHT-RADIUS)
-        self.vx = random.uniform(-5, 5)
-        self.vy = random.uniform(-5, 5)
+        self.vx = random.uniform(-MAX_SPEED, MAX_SPEED)
+        self.vy = random.uniform(-MAX_SPEED, MAX_SPEED)
         self.color = NN_POINT_COLOR
         self.fitness = 0
         self.brain = Neural_Network.NeuralNetwork()
         self.alive = True
+        self.vision = []
 
     def update(self):
 
-        move = self.brain.predict(np.random.rand(1,8))
+        move = self.brain(self.vision)
 
+        # clear vision
+        self.vision = []
+
+        #print(self.x,self.y)
         # update position
-        self.x += 10*move[0][0]
-        self.y += 10*move[0][1]
+        self.x += MAX_SPEED/2*move[0][0]
+        self.y += MAX_SPEED/2*move[0][1]
+        #print('Movement ' , move[0][0],move[0][1])
+        #print(self.x,self.y)
 
         if self.x -RADIUS < 0 or self.x + RADIUS > WIDTH:
             self.alive = False
-            print('Point left boundary')
         
         if self.y - RADIUS < 0 or self.y + RADIUS > HEIGHT:
             self.alive = False
-            print('Point left boundary')
 
-        # check for collision with walls and adjust behavior
-        # if particle interacts with wall do not it proceed past wall
-        """
-        if self.x - RADIUS < 0:
-            self.x = RADIUS
+    def check_position(self):
+        distance_from_center = np.hypot(self.x - WIDTH/2, self.y - HEIGHT/2)
+        distance_to_top = HEIGHT - self.y
+        distance_to_right = WIDTH - self.x
+        distance_to_left = self.x
+        distance_to_bottom = self.y
+        return [distance_from_center,distance_to_top,distance_to_right,distance_to_left,distance_to_bottom]
 
-        if self.x + RADIUS > WIDTH:
-            self.x = WIDTH - RADIUS
+    # reduce total vision matrix to points closest in visual field
+    # output will be tensor supplied to move execution
+    def look(self):
+        self.vision = self.vision[:VISUAL_FIELD]
+        position = self.check_position()
+        self.vision.append(position)
+        self.vision = np.array(self.vision)
+        self.vision = tf.convert_to_tensor(self.vision)
+        print(self.vision)
 
-        if self.y - RADIUS < 0:
-            self.y = RADIUS
-
-        if self.y + RADIUS > HEIGHT:
-            self.y = HEIGHT - RADIUS
-        """
     def reset_position(self):
         self.x = WIDTH/2
         self.y = HEIGHT/2
@@ -207,10 +246,12 @@ def main():
 
     pool = Network_Pool()
 
+    tf.config.run_functions_eagerly(True)
+
     while True:
     # iterate through population and run simulation
         for i in range(len(pool.population)):
-
+            print('iteration start')
             pygame.init()
             screen = pygame.display.set_mode((WIDTH, HEIGHT))
             clock = pygame.time.Clock()
@@ -234,8 +275,7 @@ def main():
 
                 # Calculate elapsed time
                 elapsed_time = pygame.time.get_ticks() - start_time
-                seconds = elapsed_time // 1000
-
+                #seconds = elapsed_time // 1000
 
                 # update and draw blue points
                 for point in collision_points:
@@ -244,32 +284,45 @@ def main():
 
                     # check for collision with red point
                     distance = np.hypot(point.x - nn_point.x, point.y - nn_point.y)
+
+                    # Creating vision matrix list that contains distance, position, and velocity information
+                    nn_point.vision.append([distance,point.x,point.y,point.vx,point.vy])
+
                     if distance < 2 * RADIUS:
-                        print(f"Blue point collided with red point at ({point.x}, {point.y})")
+                        #print(f"Blue point collided with red point at ({point.x}, {point.y})")
                         # track the neural networks performance
                         nn_point.fitness = elapsed_time
-                        running = False
+                        nn_point.alive = False
 
+                # sort the vision matrix to closest first
+                nn_point.vision = sorted(nn_point.vision, key = lambda x: x[0])
+
+                # check if point has gone out of bounds or collided with 
+                # any other points and terminate iteration if true
+                if nn_point.alive == False:
+                    #print('dead point')
+                    running = False
+
+                nn_point.look()
                 nn_point.update()
                 nn_point.draw(screen)
-
-                # check if point has gone out of bounds.
-                # terminate iteration if true
-                if nn_point.alive == False:
-                    print('dead point')
-                    running = False
 
                 pygame.display.flip()
                 clock.tick(60)
         
-            print('game end')
+            print('iteration end')
+            time.sleep(1)
             pygame.quit()
+
+        print('pop end')
+        time.sleep(1)
 
         pool.order_by_performance()
         pool.save_best()
         pool.population[0].brain = pool.crossover(pool.population[0].brain,pool.population[1].brain)
         pool.mutate_population(pool.rattle(pool.population[0].brain,POPULATION_SIZE))
-        pool.reset_population_attributes()
+        pool.reset_population()
+        
 
 
 if __name__ == "__main__":
